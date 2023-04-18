7ab63c2fbeda37b0f941c717f7a451c50f440ae7
indigo5679 <indigo5679@users.noreply.github.com>
2023-04-18 18:57:59 -0500

    Added optimzations

---
+++ app/js/data.js
@@ -0,0 +1,17 @@
+const photos = [
+    {
+        title:"Mushroom in Leaves", src: "images/image-001.jpg"
+    },
+    {
+        title:"Trees in the fall", src: "images/image-002.jpg"
+    },
+    {
+        title:"Pretty flowers", src: "images/image-003.jpg"
+    },
+    {
+        title:"Pretty trees", src: "images/image-004.jpg"
+    },
+    {
+        title:"Lake with trees around", src: "images/image-005.jpg"
+    }
+];
