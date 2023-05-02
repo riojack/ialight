8796c883c363fe4daa9d62846b73754846fa899f
indigo5679 <indigo5679@users.noreply.github.com>
2023-05-02 18:46:32 -0500

    Added meta info

--- app/js/data.js
+++ app/js/data.js
@@ -1,53 +1,53 @@
 const photos = [
     {
-        title:"Mushroom in Leaves", src: "images/image-001.jpg"
+        title:"Mushroom in Leaves", src: "images/image-001.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Trees in the fall", src: "images/image-002.jpg"
+        title:"Trees in the fall", src: "images/image-002.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Pretty flowers", src: "images/image-003.jpg"
+        title:"Pretty flowers", src: "images/image-003.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Pretty trees", src: "images/image-004.jpg"
+        title:"Pretty trees", src: "images/image-004.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-005.jpg"
+        title:"Lake with trees around", src: "images/image-005.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Sunflower", src: "images/image-006.jpg"
+        title:"Sunflower", src: "images/image-006.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-007.jpg"
+        title:"Lake with trees around", src: "images/image-007.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-008.jpg"
+        title:"Lake with trees around", src: "images/image-008.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-009.jpg"
+        title:"Lake with trees around", src: "images/image-009.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-010.jpg"
+        title:"Lake with trees around", src: "images/image-010.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-011.jpg"
+        title:"Lake with trees around", src: "images/image-011.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-012.jpg"
+        title:"Lake with trees around", src: "images/image-012.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-013.jpg"
+        title:"Lake with trees around", src: "images/image-013.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-014.jpg"
+        title:"Lake with trees around", src: "images/image-014.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-015.jpg"
+        title:"Lake with trees around", src: "images/image-015.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-        title:"Lake with trees around", src: "images/image-016.jpg"
+        title:"Lake with trees around", src: "images/image-016.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     },
     {
-    title:"Lake with trees around", src: "images/image-017.jpg"
+    title:"Lake with trees around", src: "images/image-017.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
     }
 ];
