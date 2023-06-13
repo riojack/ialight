8ce03989983c97efb7458c4b598edabaf2c3a29f
riojack <riojack@users.noreply.github.com>
2023-06-13 17:44:47 -0500

    format data, add tags, titles

--- app/js/data.js
+++ app/js/data.js
@@ -1,53 +1,53 @@
 const photos = [
     {
-        title:"Mushroom in Leaves", src: "images/image-001.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Mushroom in Leaves", src: "images/image-001.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Trees in the fall", src: "images/image-002.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Trees in the fall", src: "images/image-002.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Pretty flowers", src: "images/image-003.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Pretty flowers", src: "images/image-003.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Pretty trees", src: "images/image-004.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Pretty trees", src: "images/image-004.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Lake with trees around", src: "images/image-005.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Lake with trees around", src: "images/image-005.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Sunflower", src: "images/image-006.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Sunflower", src: "images/image-006.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Lake with trees around", src: "images/image-007.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Blue Dragonfly", src: "images/image-007.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Lake with trees around", src: "images/image-008.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "On I-35 Northbound", src: "images/image-008.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:on the road"],
     },
     {
-        title:"Lake with trees around", src: "images/image-009.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Butterfly on a flower", src: "images/image-009.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:bug"],
     },
     {
-        title:"Lake with trees around", src: "images/image-010.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Peering over the edge", src: "images/image-010.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:bug"],
     },
     {
-        title:"Lake with trees around", src: "images/image-011.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Burst of Sunset", src: "images/image-011.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Lake with trees around", src: "images/image-012.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Tunnel of Trees", src: "images/image-012.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Lake with trees around", src: "images/image-013.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Barn Bridge", src: "images/image-013.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Lake with trees around", src: "images/image-014.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Babbling Brook", src: "images/image-014.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-        title:"Lake with trees around", src: "images/image-015.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Cucumber Beetle", src: "images/image-015.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:bugs"],
     },
     {
-        title:"Lake with trees around", src: "images/image-016.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "A Friend of All", src: "images/image-016.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     },
     {
-    title:"Lake with trees around", src: "images/image-017.jpg", camera:"Canon EOS 80D", lens:"-", datetime:"April 5th, 1984"
+        title: "Purples and Blues", src: "images/image-017.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "April 5th, 1984", tags: ["camera:Canon EOS80D", "t:nature"],
     }
 ];
