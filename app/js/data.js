9ebcba1a62e230357447d4d4edab57642cf0ede2
riojack <riojack@users.noreply.github.com>
2023-10-03 16:36:05 -0500

    refactor: format all code files by EditorConfig rules

--- app/js/data.js
+++ app/js/data.js
@@ -1,53 +1,53 @@
 const photos = [
-    {
-        title: "Mushroom in Leaves", src: "images/image-001.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "September 10th, 2017", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Trees in the fall", src: "images/image-002.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "October 9th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Pretty flowers", src: "images/image-003.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 30th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Pretty trees", src: "images/image-004.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 30th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Lake with trees around", src: "images/image-005.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 12th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Sunflower", src: "images/image-006.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 6th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Blue Dragonfly", src: "images/image-007.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "July 5th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "On I-35 Northbound", src: "images/image-008.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "May 2nd, 2018", tags: ["camera:Canon EOS80D", "t:on the road"],
-    },
-    {
-        title: "Butterfly on a flower", src: "images/image-009.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 16th, 2017", tags: ["camera:Canon EOS80D", "t:bugs"],
-    },
-    {
-        title: "Peering over the edge", src: "images/image-010.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "June 16th, 2017", tags: ["camera:Canon EOS80D", "t:bugs"],
-    },
-    {
-        title: "Burst of Sunset", src: "images/image-011.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "June 14th, 2017", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Tunnel of Trees", src: "images/image-012.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "February 19th, 2017", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Barn Bridge", src: "images/image-013.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "October 9th, 2016", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Babbling Brook", src: "images/image-014.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "September 10th, 2016", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Cucumber Beetle", src: "images/image-015.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 28th, 2016", tags: ["camera:Canon EOS80D", "t:bugs"],
-    },
-    {
-        title: "A Friend of All", src: "images/image-016.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "July 9th, 2016", tags: ["camera:Canon EOS80D", "t:nature"],
-    },
-    {
-        title: "Purples and Blues", src: "images/image-017.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "July 17, 2016", tags: ["camera:Canon EOS80D", "t:nature"],
-    }
+  {
+    title: "Mushroom in Leaves", src: "images/image-001.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "September 10th, 2017", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Trees in the fall", src: "images/image-002.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "October 9th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Pretty flowers", src: "images/image-003.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 30th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Pretty trees", src: "images/image-004.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 30th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Lake with trees around", src: "images/image-005.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 12th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Sunflower", src: "images/image-006.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 6th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Blue Dragonfly", src: "images/image-007.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "July 5th, 2018", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "On I-35 Northbound", src: "images/image-008.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "May 2nd, 2018", tags: ["camera:Canon EOS80D", "t:on the road"],
+  },
+  {
+    title: "Butterfly on a flower", src: "images/image-009.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 16th, 2017", tags: ["camera:Canon EOS80D", "t:bugs"],
+  },
+  {
+    title: "Peering over the edge", src: "images/image-010.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "June 16th, 2017", tags: ["camera:Canon EOS80D", "t:bugs"],
+  },
+  {
+    title: "Burst of Sunset", src: "images/image-011.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "June 14th, 2017", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Tunnel of Trees", src: "images/image-012.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "February 19th, 2017", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Barn Bridge", src: "images/image-013.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "October 9th, 2016", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Babbling Brook", src: "images/image-014.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "September 10th, 2016", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Cucumber Beetle", src: "images/image-015.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "August 28th, 2016", tags: ["camera:Canon EOS80D", "t:bugs"],
+  },
+  {
+    title: "A Friend of All", src: "images/image-016.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "July 9th, 2016", tags: ["camera:Canon EOS80D", "t:nature"],
+  },
+  {
+    title: "Purples and Blues", src: "images/image-017.jpg", camera: "Canon EOS 80D", lens: "-", datetime: "July 17, 2016", tags: ["camera:Canon EOS80D", "t:nature"],
+  }
 ];
--- app/js/events.js
+++ app/js/events.js
@@ -1,21 +1,23 @@
 function attachHandlers(/** @type {HTMLElement} */sideBarDOM) {
-    const hamburger_anchor = sideBarDOM.querySelector("a.sidebar-opener");
-    const sidebar_closer_anchor = sideBarDOM.querySelector("a.sidebar-closer");
-    hamburger_anchor.addEventListener("touchend", _sideBarMouseEnter)
-    hamburger_anchor.addEventListener("click", _sideBarMouseEnter)
-    sidebar_closer_anchor.addEventListener("click", _sidebarClose)
-    sidebar_closer_anchor.addEventListener("touchend", _sidebarClose)
-};
+  const hamburger_anchor = sideBarDOM.querySelector("a.sidebar-opener");
+  const sidebar_closer_anchor = sideBarDOM.querySelector("a.sidebar-closer");
+  hamburger_anchor.addEventListener("touchend", _sideBarMouseEnter);
+  hamburger_anchor.addEventListener("click", _sideBarMouseEnter);
+  sidebar_closer_anchor.addEventListener("click", _sidebarClose);
+  sidebar_closer_anchor.addEventListener("touchend", _sidebarClose);
+}
+
 function _sideBarMouseEnter(/** @type {MouseEvent} */e) {
-    document.body.className = "sideBar-open"
-    e.preventDefault();
-};
+  document.body.className = "sideBar-open";
+  e.preventDefault();
+}
+
 function _sideBarMouseLeave() {
-    document.body.className = ""
-};
+  document.body.className = "";
+}
+
 function _sidebarClose(/** @type {MouseEvent} */e) {
+  document.body.className = "";
 
-    document.body.className = "";
-
-    e.preventDefault();
+  e.preventDefault();
 }
