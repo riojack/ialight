ed0925e624bdff4119272fba1b4d3ec9b32258f2
indigo5679 <indigo5679@users.noreply.github.com>
2023-11-28 18:59:24 -0600

    Improved search

--- app/js/events.js
+++ app/js/events.js
@@ -9,6 +9,13 @@ function attachHandlers( /** @type {HTMLElement} */ sideBarDOM, /** @type {HTMLE
     sidebar_closer_anchor.addEventListener("click", _sidebarClose);
     sidebar_closer_anchor.addEventListener("touchend", _sidebarClose);
     searchBar.addEventListener("keyup", _handleSearch);
+
+    const modal = document.getElementById("image-modal");
+    modal.addEventListener("show.bs.modal", (e) => {
+        const src = e.relatedTarget.src || e.relatedTarget.dataset.src;
+
+        document.getElementById("img-to-show").setAttribute("src", src);
+    });
 }
 
 function _sideBarMouseEnter( /** @type {MouseEvent} */ e) {
@@ -27,7 +34,7 @@ function _sidebarClose( /** @type {MouseEvent} */ e) {
 
 function _handleSearch( /** @type {KeyboardEvent} */ event) {
     const searchValue = event.currentTarget.value
-    const results = doSearch(searchValue, 4);
+    const results = doSearch(searchValue, 10);
     console.log(results)
     const container = document.querySelector("#search-results");
     container.innerHTML = "";
@@ -35,9 +42,15 @@ function _handleSearch( /** @type {KeyboardEvent} */ event) {
     for (let i = 0; i < results.length; i++) {
         const item = document.createElement("li");
         const anchor = document.createElement("a");
-        const image = document.createElement("img")
+        const image = document.createElement("img");
+        const title = document.createElement("p");
+        title.textContent = results[i].title;
+        anchor.dataset.src = results[i].src;
+        anchor.setAttribute("data-bs-toggle", "modal");
+        anchor.setAttribute("data-bs-target", "#image-modal");
         image.src = results[i].src
         anchor.appendChild(image)
+        anchor.appendChild(title)
         item.appendChild(anchor)
         list.appendChild(item)
     }
