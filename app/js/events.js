67387e5a01869b52ddf0561417bad595b334e9e4
indigo5679 <indigo5679@users.noreply.github.com>
2024-05-07 18:27:14 -0500

    Fixed tag page

--- app/js/events.js
+++ app/js/events.js
@@ -1,58 +1,59 @@
 function attachHandlers( /** @type {HTMLElement} */ sideBarDOM, /** @type {HTMLElement} */ searchBarDOM) {
-  const hamburger_anchor = sideBarDOM.querySelector("a.sidebar-opener");
-  const sidebar_closer_anchor = sideBarDOM.querySelector("a.sidebar-closer");
-  const searchBar = searchBarDOM.querySelector("#search-input");
+    const hamburger_anchor = sideBarDOM.querySelector("a.sidebar-opener");
+    const sidebar_closer_anchor = sideBarDOM.querySelector("a.sidebar-closer");
 
-  hamburger_anchor.addEventListener("touchend", _sideBarMouseEnter);
-  hamburger_anchor.addEventListener("click", _sideBarMouseEnter);
+    hamburger_anchor.addEventListener("touchend", _sideBarMouseEnter);
+    hamburger_anchor.addEventListener("click", _sideBarMouseEnter);
 
-  sidebar_closer_anchor.addEventListener("click", _sidebarClose);
-  sidebar_closer_anchor.addEventListener("touchend", _sidebarClose);
-  searchBar.addEventListener("keyup", _handleSearch);
-
-  const modal = document.getElementById("image-modal");
-  modal.addEventListener("show.bs.modal", (e) => {
-    const src = e.relatedTarget.src || e.relatedTarget.dataset.src;
-    document.getElementById("img-to-show").focus();
-    document.getElementById("img-to-show").setAttribute("src", src);
-  });
+    sidebar_closer_anchor.addEventListener("click", _sidebarClose);
+    sidebar_closer_anchor.addEventListener("touchend", _sidebarClose);
+    if (searchBarDOM) {
+        const searchBar = searchBarDOM.querySelector("#search-input");
+        searchBar.addEventListener("keyup", _handleSearch);
+    }
+    const modal = document.getElementById("image-modal");
+    modal.addEventListener("show.bs.modal", (e) => {
+        const src = e.relatedTarget.src || e.relatedTarget.dataset.src;
+        document.getElementById("img-to-show").focus();
+        document.getElementById("img-to-show").setAttribute("src", src);
+    });
 }
 
 function _sideBarMouseEnter( /** @type {MouseEvent} */ e) {
-  document.body.className = "sideBar-open";
-  e.preventDefault();
+    document.body.className = "sideBar-open";
+    e.preventDefault();
 }
 
 function _sideBarMouseLeave() {
-  document.body.className = "";
+    document.body.className = "";
 }
 
 function _sidebarClose( /** @type {MouseEvent} */ e) {
-  document.body.className = "";
-  e.preventDefault();
+    document.body.className = "";
+    e.preventDefault();
 }
 
 function _handleSearch( /** @type {KeyboardEvent} */ event) {
-  const searchValue = event.currentTarget.value;
-  const results = doSearch(searchValue, 10);
-  const container = document.querySelector("#search-results");
-  container.innerHTML = "";
-  const list = document.createElement("ul");
+    const searchValue = event.currentTarget.value;
+    const results = doSearch(searchValue, 10);
+    const container = document.querySelector("#search-results");
+    container.innerHTML = "";
+    const list = document.createElement("ul");
 
-  for (let i = 0; i < results.length; i++) {
-    const item = document.createElement("li");
-    const anchor = document.createElement("a");
-    const image = document.createElement("img");
-    const title = document.createElement("p");
-    title.textContent = results[i].title;
-    anchor.dataset.src = results[i].src;
-    anchor.setAttribute("data-bs-toggle", "modal");
-    anchor.setAttribute("data-bs-target", "#image-modal");
-    image.src = results[i].src;
-    anchor.appendChild(image);
-    anchor.appendChild(title);
-    item.appendChild(anchor);
-    list.appendChild(item);
-  }
-  container.appendChild(list);
+    for (let i = 0; i < results.length; i++) {
+        const item = document.createElement("li");
+        const anchor = document.createElement("a");
+        const image = document.createElement("img");
+        const title = document.createElement("p");
+        title.textContent = results[i].title;
+        anchor.dataset.src = results[i].src;
+        anchor.setAttribute("data-bs-toggle", "modal");
+        anchor.setAttribute("data-bs-target", "#image-modal");
+        image.src = results[i].src;
+        anchor.appendChild(image);
+        anchor.appendChild(title);
+        item.appendChild(anchor);
+        list.appendChild(item);
+    }
+    container.appendChild(list);
 }
