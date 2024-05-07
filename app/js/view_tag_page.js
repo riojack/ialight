b3e5cd8ee44760a88025d4f8221f529de6d991ca
indigo5679 <indigo5679@users.noreply.github.com>
2024-05-07 18:40:08 -0500

    Fixed gridding on tags

--- app/js/view_tag_page.js
+++ app/js/view_tag_page.js
@@ -1,42 +1,43 @@
-function setupViewTagPage(/** @type {Document} */ doc, /** @type {Window} */ wndw, /** @type {Array} */listOfPhotos) {
-  const modal = doc.getElementById("image-modal");
-  const grid = doc.querySelector(".tag-image-grid");
+function setupViewTagPage( /** @type {Document} */ doc, /** @type {Window} */ wndw, /** @type {Array} */ listOfPhotos) {
+    const modal = doc.getElementById("image-modal");
+    const grid = doc.querySelector(".app-container");
 
-  const params = new URLSearchParams(wndw.location.search);
-  const tagToSearchFor = "t:" + params.get("tag");
-  const photosWithTag = [];
+    const params = new URLSearchParams(wndw.location.search);
+    const tagToSearchFor = "t:" + params.get("tag");
+    const photosWithTag = [];
 
-  // Find Photos with tagToSearchFor
-  for (const photo of listOfPhotos) {
-    const photoTags = photo.tags;
+    // Find Photos with tagToSearchFor
+    for (const photo of listOfPhotos) {
+        const photoTags = photo.tags;
 
-    if (photoTags.indexOf(tagToSearchFor) >= 0) {
-      photosWithTag.push(photo);
+        if (photoTags.indexOf(tagToSearchFor) >= 0) {
+            photosWithTag.push(photo);
+        }
     }
-  }
 
-  modal.addEventListener("show.bs.modal", (e) => {
-    const src = e.relatedTarget.src;
+    modal.addEventListener("show.bs.modal", (e) => {
+        const src = e.relatedTarget.src;
 
-    doc.getElementById("img-to-show").setAttribute("src", src);
-  });
+        doc.getElementById("img-to-show").setAttribute("src", src);
+    });
 
-  // Show the matching photos on screen
-  for (const photo of photosWithTag) {
-    const container = doc.createElement("div");
-    const img = doc.createElement("img");
-    const anchor = doc.createElement("a");
+    // Show the matching photos on screen
+    for (let i = photosWithTag.length - 1; i >= 0; i--) {
+        const photo = photosWithTag[i];
+        const container = doc.createElement("div");
+        const img = doc.createElement("img");
+        const anchor = doc.createElement("a");
+        container.setAttribute("class", "thumbnail");
 
-    anchor.setAttribute("href", "/");
-    anchor.addEventListener("click", doNothing);
-    img.setAttribute("src", photo.src);
-    img.setAttribute("data-bs-toggle", "modal");
-    img.setAttribute("data-bs-target", "#image-modal");
-    anchor.appendChild(img);
+        anchor.setAttribute("href", "/");
+        anchor.addEventListener("click", doNothing);
+        img.setAttribute("src", photo.src);
+        img.setAttribute("data-bs-toggle", "modal");
+        img.setAttribute("data-bs-target", "#image-modal");
+        anchor.appendChild(img);
 
-    container.appendChild(anchor);
+        container.appendChild(anchor);
 
-    grid.appendChild(container);
-  }
+        grid.insertAdjacentElement("afterbegin", container);
+    }
 }
-
