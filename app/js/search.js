579998e5d56517cfdb8721ac053c6d756925bac9
riojack <riojack@users.noreply.github.com>
2024-05-06 21:41:05 -0500

    update search visuals

--- app/js/search.js
+++ app/js/search.js
@@ -1,14 +1,14 @@
 function doSearch(searchTerm, resultLimit) {
+  const searchTermLc = searchTerm.toLowerCase();
   const results = [];
   for (let i = 0; i < photos.length; i++) {
     if (i === resultLimit) {
       break;
     }
     const photo = photos[i];
-    if (photo.title.toLowerCase().indexOf(searchTerm) >= 0) {
+    if (photo.title.toLowerCase().indexOf(searchTermLc) >= 0) {
       results.push(photo);
     }
-
   }
   return results;
 }
